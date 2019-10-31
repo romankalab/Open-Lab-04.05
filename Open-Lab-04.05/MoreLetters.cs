5f40a34bea6beda6986f3fce009c25fdb0681619
romankalab <roan.kalab@mail.com>
2019-11-01 00:20:00 +0100

    Initial

---
+++ Open-Lab-04.05/MoreLetters.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Letters
+{
+    class MoreLetters
+    {
+        public string GetNewWord(string original, int howmanytimes)
+        {
+            string result = "";
+            for (var x = 0; x < original.Length; x++)
+                for (var y = 0; y < howmanytimes; y++)
+                    result = result + original[x];
+            return result;
+        }
+    }
+}
---
+++ Open-Lab-04.05/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Letters;
+
+namespace Open_Lab_04._05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter word:");
+            string original = Console.ReadLine();
+
+            Console.WriteLine("Enter number:");
+            int howmanytimes = Convert.ToInt32(Console.ReadLine());
+
+            MoreLetters moreletters = new MoreLetters();
+            Console.WriteLine(moreletters.GetNewWord(original, howmanytimes));
+        }
+    }
+}
